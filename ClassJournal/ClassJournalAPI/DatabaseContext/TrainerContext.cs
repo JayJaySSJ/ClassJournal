c854388c003d31b76fa7bc6b0304b48c7699fd86
JayJaySSJ <jakub.luczejko@gmail.com>
2023-08-07 18:16:06 +0200

    created initial project items for db

---
+++ ClassJournal/ClassJournalAPI/DatabaseContext/TrainerContext.cs
@@ -0,0 +1,15 @@
+﻿using ClassJournal.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ClassJournal.API.DatabaseContext
+{
+    public class TrainerContext : DbContext
+    {
+        public DbSet<TrainerModel> Trainers { get; set; }
+
+        public TrainerContext(DbContextOptions contextOptions) : base(contextOptions)
+        {
+        
+        }
+    }
+}
