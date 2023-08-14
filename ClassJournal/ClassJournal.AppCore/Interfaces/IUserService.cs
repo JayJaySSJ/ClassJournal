28fca3d69cda6a76a41f109f9fbc1416c40a39cd
JayJaySSJ <jakub.luczejko@gmail.com>
2023-08-14 17:29:18 +0200

    created AppCore with repo and CRUDs adjustment

---
+++ ClassJournal/ClassJournal.AppCore/Interfaces/IUserService.cs
@@ -0,0 +1,13 @@
+﻿using ClassJournal.AppCore.Models;
+
+namespace ClassJournal.AppCore.Interfaces
+{
+    public interface IUserService
+    {
+        IEnumerable<UserModel> GetAll();
+        UserModel Get(int id);
+        void Add(UserModel entity);
+        void Update(UserModel dbEntity, UserModel entity);
+        void Delete(UserModel entity);
+    }
+}
